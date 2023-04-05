0096559f99c3c2fdc62842b389c460e14c9e2f6e
Chpatel7085 <126132633+Chpatel7085@users.noreply.github.com>
2023-04-05 16:31:51 +0530

    added graphics header and cpp files

---
+++ OpenGlTutorial/OpenGlTutorial/src/graphics/shader.cpp
@@ -0,0 +1,83 @@
+#include "shader.h"
+
+Shader::Shader() {}
+
+void Shader::generate(const char* vertexShaderPath, const char* fragShaderPath) {
+	generate(vertexShaderPath, fragShaderPath);
+}
+
+Shader::Shader(const char* vertexShaderPath, const char* fragmentShaderPath) {
+	int success;
+	char infoLog[512];
+
+	GLuint vertexShader = compileShader(vertexShaderPath, GL_VERTEX_SHADER);
+	GLuint fragShader = compileShader(fragmentShaderPath, GL_FRAGMENT_SHADER);
+	id = glCreateProgram();
+	glAttachShader(id, vertexShader);
+	glAttachShader(id, fragShader);
+	glLinkProgram(id);
+
+	//catch errors
+	glGetProgramiv(id, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(id, 512, NULL, infoLog);
+		std::cout << "Linking error: " << infoLog << std::endl;
+	}
+
+
+	//Deleting the vertex and frag as we have already linked to it
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragShader);
+}
+
+void Shader::activate() {
+	glUseProgram(id);
+}
+
+
+std::string Shader:: loadShaderSrc(const char* filename) {
+	std::ifstream file;
+	std::stringstream buf;// to read the data of the file
+	std::string ret = "";//placeholder for the return variable
+
+	file.open(filename);
+
+	if (file.is_open()) {
+		buf << file.rdbuf();//read buffer/ piping to the string buffer
+		ret = buf.str();
+
+	}
+	else {
+		std::cout << "Could not open " << filename << std::endl;
+	}
+	file.close();
+	return ret;
+}
+
+GLuint Shader::compileShader(const char* filepath, GLenum type) {
+	int success;
+	char infoLog[512];
+
+	GLuint ret = glCreateShader(type);
+	std::string shaderSrc = loadShaderSrc(filepath);
+	const GLchar* shader = shaderSrc.c_str();
+	glShaderSource(ret, 1, &shader, NULL);
+	glCompileShader(ret);
+
+	//errors
+	glGetShaderiv(ret, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(ret, 512, NULL, infoLog);
+		std::cout << "Error with vertex shader comp : " << infoLog << std::endl;
+	}
+
+	return ret;
+}
+
+void Shader::setMat4(const std::string& name, glm::mat4 val) {
+	glUniformMatrix4fv(glGetUniformLocation(id, name.c_str()), 1, GL_FALSE, glm::value_ptr(val));
+}
+
+void Shader::setInt(const std::string& name, int value) {
+	glUniform1i(glGetUniformLocation(id, name.c_str()), value);
+}
